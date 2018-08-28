09198a73ee64cfc9b6d8e5056e2eee292fa08c28
Oleksandr Pavliuchyk <opavliuchyk@rubiconproject.com>
2018-08-28 16:50:20 +0300

    A- Setup and create ad units

--- DemoApp/src/main/java/org/prebid/mobile/demoapp/PrebidApplication.java
+++ DemoApp/src/main/java/org/prebid/mobile/demoapp/PrebidApplication.java
@@ -4,6 +4,19 @@ import android.app.Application;
 import android.os.Build;
 import android.webkit.WebView;
 
+import org.prebid.mobile.core.AdUnit;
+import org.prebid.mobile.core.BannerAdUnit;
+import org.prebid.mobile.core.InterstitialAdUnit;
+import org.prebid.mobile.core.TargetingParams;
+
+import java.util.ArrayList;
+
+import static org.prebid.mobile.demoapp.Constants.BANNER_300x250;
+import static org.prebid.mobile.demoapp.Constants.BANNER_320x50;
+import static org.prebid.mobile.demoapp.Constants.INTERSTITIAL_FULLSCREEN;
+import static org.prebid.mobile.demoapp.Constants.PBS_CONFIG_300x250_RUBICON_DEMAND;
+import static org.prebid.mobile.demoapp.Constants.PBS_CONFIG_RUBICON_DEMAND;
+
 public class PrebidApplication extends Application {
     /**
      * Called when the application is starting, before any activity, service,
@@ -26,6 +39,44 @@ public class PrebidApplication extends Application {
             WebView.setWebContentsDebuggingEnabled(true);
         }
 
+        /**
+         * Initialise prebid for DFP ad unit
+         */
+        initialisePrebidForDFP();
+
+    }
+
+    private void initialisePrebidForDFP() {
+
+        //A- Setup and create ad units
+        ArrayList<AdUnit> adUnits = new ArrayList<AdUnit>();
+
+        //Configure Ad-Slot1
+        BannerAdUnit adUnit1 = new BannerAdUnit(BANNER_320x50, PBS_CONFIG_RUBICON_DEMAND);
+        adUnit1.addSize(320, 50);
+
+        //Configure Ad-Slot2 with the same demand source
+        BannerAdUnit adUnit2 = new BannerAdUnit(BANNER_300x250, PBS_CONFIG_300x250_RUBICON_DEMAND);
+        adUnit2.addSize(300, 250);
+        adUnit2.addSize(300, 600);
+
+        //Configure Interstitial Ad Unit
+        InterstitialAdUnit adUnit3 = new InterstitialAdUnit(INTERSTITIAL_FULLSCREEN, PBS_CONFIG_RUBICON_DEMAND);
+
+        // Add Configuration
+        adUnits.add(adUnit1);
+        adUnits.add(adUnit2);
+        adUnits.add(adUnit3);
+
+        // Set targeting
+        TargetingParams.setGender(TargetingParams.GENDER.FEMALE);
+        TargetingParams.setYearOfBirth(1992);
+        TargetingParams.setLocationDecimalDigits(2);
+        TargetingParams.setLocationEnabled(true);
+        TargetingParams.setUserTargeting("PrebidKey", "PrebidValue"); // this should add "Prebidkey=PrebidValue" in user.keywords in ortb request
+        TargetingParams.setUserTargeting("PrebidKey2", null); // this should add "PrebidKey2" in user.keywords in ortb request
+        TargetingParams.setUserTargeting(null, "PrebidValue2"); // this should add nothing
+        TargetingParams.setUserTargeting(null, null); // this should add nothing
     }
 
 }
