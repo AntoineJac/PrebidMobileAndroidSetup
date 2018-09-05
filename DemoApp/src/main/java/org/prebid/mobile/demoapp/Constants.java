54a8530cf2931aa16d4acb37e18561f28a9cf5ea
Oleksandr Pavliuchyk <opavliuchyk@rubiconproject.com>
2018-09-05 11:47:38 +0300

    RetailMeNot 15946 Publisher parameters were added

--- DemoApp/src/main/java/org/prebid/mobile/demoapp/Constants.java
+++ DemoApp/src/main/java/org/prebid/mobile/demoapp/Constants.java
@@ -3,8 +3,8 @@ package org.prebid.mobile.demoapp;
 public class Constants {
 
     // Banner Content
-    public static final String BANNER_320x50 = "INSERT-AD-UNIT-ID-HERE";
-    public static final String BANNER_300x250 = "INSERT-AD-UNIT-ID-HERE";
+    public static final String BANNER_320x50 = "Banner_320x50";
+    public static final String BANNER_300x250 = "Banner_300x250";
 
     // Interstitial Content
     public static final String INTERSTITIAL_FULLSCREEN = "INSERT-AD-UNIT-ID-HERE";
@@ -18,15 +18,17 @@ public class Constants {
 
     // DFP Banner
     public static final String DFP_BANNER_ADUNIT_ID_320x50 = "INSERT-DFP-CONFIG-ID-HERE";
-    public static final String DFP_BANNER_ADUNIT_ID_300x250 = "INSERT-DFP-CONFIG-ID-HERE";
+    //    public static final String DFP_BANNER_ADUNIT_ID_300x250 = "/31179041/coupon_area";
+    public static final String DFP_BANNER_ADUNIT_ID_300x250 = "/31179041/app_storepg_btf";
 
     // DFP Interstitial
     public static final String DFP_INTERSTITIAL_ADUNIT_ID_FULLSCREEN = "INSERT-DFP-CONFIG-ID-HERE";
 
     // PBS Config
     public static final String PBS_CONFIG_RUBICON_DEMAND = "INSERT-CONFIG-ID-HERE";
-    public static final String PBS_CONFIG_300x250_RUBICON_DEMAND = "INSERT-CONFIG-ID-HERE";
-    public static final String PBS_ACCOUNT_ID = "INSERT-ACCOUNT-ID-HERE";
+    //    public static final String PBS_CONFIG_300x250_RUBICON_DEMAND = "15946-Coupon_Area_And-300x250";
+    public static final String PBS_CONFIG_300x250_RUBICON_DEMAND = "15946-Storefront_BTF_And-300x250";
+    public static final String PBS_ACCOUNT_ID = "15946";
     // App Constants
     public static final String ADSERVER = "AdServerName";
     public static final String ADFORMAT = "AdFormat";
